a3ef6eeee0c11d52f124378d31e6d115291e9b4d
thedatabaseme <philip.haberkern@googlemail.com>
2022-05-27 10:40:26 +0200

    Added clone

---
+++ terraform/proxmox/clone_vm/clone_vm.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "<PROXMOX_API_URL>"         # Proxmox API endpoint e.g. https://proxmox-server01.example.com:8006/api2/json
+  pm_api_token_id     = "<PROXMOX_API_TOKEN_ID>"    # Generate API Token using the Proxmox WebUI
+  pm_api_token_secret = "<PROXMOX_API_SECRET>"      # Secret access key, shown when generating the token using the Proxmox WebUI
+
+  # Uncomment when using a self signed certificate
+  # pm_tls_insecure     = true
+
+  # Logging section, uncomment for enable logging
+  # pm_log_enable       = true
+  # pm_log_file         = proxmox_terraform.log
+}
+
+# Create a new blank VM
+resource "proxmox_vm_qemu" "test-vm" {
+  name        = "test-vm"
+  target_node = "<PROXMOX_NODE>"
+  clone       = "<SOURCE_CLONE_NAME>"   # VM name to clone from
+  agent       = 1                       # 1 to enable QEMU agent
+
+  memory      = 1024
+  sockets     = 1
+  cores       = 2
+}
